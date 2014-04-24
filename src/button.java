a1204670b17d31dab01debaafdd87d83681b1af3
Sergey <s.bovtruk@gmail.com>
2014-04-24 18:42:35 +0300

    Update and rename ElemName2.java to button.java

---
+++ src/button.java
@@ -0,0 +1,18 @@
+public class Button extends ElementSettings {
+
+  public Integer width;
+
+  public Integer height;
+  
+  public Integer zIndex;
+
+  public void getWidth() {
+  }
+
+  public void getHeight() {
+  }
+  
+  public void getzIndex() {
+  }
+
+}
